8fe639999248cb6602dfee3f4fda53d3cea9c49b
Renan <qs.zero@gmail.com>
2025-08-08 21:17:40 -0300

    Final Project

--- database/migrations/001_add_banner_top_url.sql
+++
@@ -1,8 +0,0 @@
--- Migration para adicionar campo banner_top_url à tabela Filmes
--- Execute este script no banco de dados para atualizar a estrutura existente
-
-ALTER TABLE "Filmes" 
-ADD COLUMN banner_top_url VARCHAR(255);
-
--- Opcional: adicionar comentário para documentar o campo
-COMMENT ON COLUMN "Filmes".banner_top_url IS 'URL da imagem horizontal do banner/poster do filme';
